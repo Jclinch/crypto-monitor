21631b2554325502a4c1885c6c13b975cb1771d6
sunny <sunnyugwu2011@gmail.com>
2025-08-02 21:47:02 +0100

    service_role

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,16 +1,17 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  process.env.SUPABASE_URL!,
+//   process.env.SUPABASE_ANON_KEY!
+  process.env.SUPABASE_SERVICE_ROLE!
 );
 
 
 
 // import { createBrowserClient } from "@supabase/ssr";
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-// const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+// const supabaseUrl = process.env.SUPABASE_URL;
+// const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 // if (!supabaseUrl || !supabaseKey) {
 //   throw new Error("Supabase environment variables are not set.");
